b5484f38e35961d57224d332462d446a80b441fc
Mateusz Jałowiczor <mateuszjalowiczor@MacBook-Air-Mateusz.local>
2022-10-06 21:17:58 +0200

    Add Book model

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import datetime
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookstore.db'
+db = SQLAlchemy(app)
+db.app = app
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route("/")
 def hello_world():
